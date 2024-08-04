e10baa5d79a854f6b01607043b9dcaa763a778c5
Angie Rodriguez <angie.rodriguez.167@gmail.com>
2024-08-04 17:07:12 -0500

    initiial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+Console.WriteLine(greeting);
+Console.WriteLine("Please enter a product name: ");
+
+
+
+string response = Console.ReadLine();
+Console.WriteLine(@$"You chose: {response}. 
+Thank you for your input!");
